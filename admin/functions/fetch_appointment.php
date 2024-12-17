72850c4a36a12ef839fad216518352f27dac8cc7
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2024-12-18 04:52:39 +0800

    asd
    dasdas

--- admin/functions/fetch_appointment.php
+++ admin/functions/fetch_appointment.php
@@ -19,21 +19,33 @@ if (isset($_POST['fetch_appointment'])) {
 
 
     while ($row = $results->fetch_assoc()) {
- 
+        if ($row['status'] == '0' && $row['doc_id'] == "") {
+            $color = 'blue';
+        }
+        if ($row['status'] == '0' && $row['doc_id'] != "") {
+            $color = 'yellow';
+        }
+        if ($row['status'] == '1'  ) {
+            $color = 'green';
+        }
         $events[] = array(
             'id' => $row['id'],                       // Unique identifier for the event
             'title' => $row['pet_name'],                // Status or description of the event
             'start' => $row['date'],       // Date of the attendance
-             // 'masa' => date("Y-m-d H:i:s", strtotime("now")),
-             'allDay' => 'true',
-             'owner_contact' =>  $row['owner_contact'],
-             'owner_name' =>  $row['owner_name2'],
-             'owner_email' =>  $row['pemail'],
-             'pet_name' => $row['pet_name'],
-             'pet_age' => $row['age'],
-             'pet_gender' => $row['gender'],
-             'pet_breed' => $row['breed'],
-             'detail' => $row['service_ids'],
+            // 'masa' => date("Y-m-d H:i:s", strtotime("now")),
+            'allDay' => 'true',
+            'owner_contact' => $row['owner_contact'],
+            'owner_name' => $row['owner_name2'],
+            'owner_email' => $row['pemail'],
+            'pet_name' => $row['pet_name'],
+            'pet_age' => $row['age'],
+            'pet_gender' => $row['gender'],
+            'pet_breed' => $row['breed'],
+            'detail' => $row['service_ids'],
+            'doc_id' => $row['doc_id'],
+            'status' => $row['status'],
+            'color' => $color,
+
             // Optionally add 'end' or other event properties here
         );
     }
@@ -64,21 +76,32 @@ if (isset($_POST['fetch_appointment_patient'])) {
 
 
     while ($row = $results->fetch_assoc()) {
- 
+        if ($row['status'] == '0' && $row['doc_id'] == "") {
+            $color = 'blue';
+        }
+        if ($row['status'] == '0' && $row['doc_id'] != "") {
+            $color = 'yellow';
+        }
+        if ($row['status'] == '1'  ) {
+            $color = 'green';
+        }
         $events[] = array(
             'id' => $row['id'],                       // Unique identifier for the event
             'title' => $row['pet_name'],                // Status or description of the event
             'start' => $row['date'],       // Date of the attendance
-             // 'masa' => date("Y-m-d H:i:s", strtotime("now")),
-             'allDay' => 'true',
-             'owner_contact' =>  $row['owner_contact'],
-             'owner_name' =>  $row['owner_name2'],
-             'owner_email' =>  $row['pemail'],
-             'pet_name' => $row['pet_name'],
-             'pet_age' => $row['age'],
-             'pet_gender' => $row['gender'],
-             'pet_breed' => $row['breed'],
-             'detail' => $row['service_ids'],
+            // 'masa' => date("Y-m-d H:i:s", strtotime("now")),
+            'allDay' => 'true',
+            'owner_contact' => $row['owner_contact'],
+            'owner_name' => $row['owner_name2'],
+            'owner_email' => $row['pemail'],
+            'pet_name' => $row['pet_name'],
+            'pet_age' => $row['age'],
+            'pet_gender' => $row['gender'],
+            'pet_breed' => $row['breed'],
+            'detail' => $row['service_ids'],
+            'doc_id' => $row['doc_id'],
+            'status' => $row['status'],
+            'color' => $color,
             // Optionally add 'end' or other event properties here
         );
     }
